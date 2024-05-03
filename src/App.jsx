adf1201e8d818d54cd5266f95522350d1ec557da
Viswabhi <avishek.viswakarma@gmail.com>
2024-05-03 23:36:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+import Header from './assets/Components/Header'
+import Body from './assets/Components/Body'
+import Footer from './assets/Components/Footer'
+import Home from './assets/Components/Home'
+
+
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+     <Header></Header>
+     <Home></Home>
+     <Footer></Footer>
+     <body></body>
+    </>
+  )
+}
+
+export default App
